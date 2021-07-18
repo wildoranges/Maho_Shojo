f6fcb9f61c1300183eb179a60746cc9b04c63aed
wildoranges <1294214414@qq.com>
2021-07-18 15:13:12 +0800

    fix ir output && fixme:double free

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 void print_help(std::string exe_name) {
   std::cout << "Usage: " << exe_name
-            << " [ -h | --help ] [ -p | --trace_parsing ] [ -s | --trace_scanning ] [ -emit-mir ] [ -e | --emit_syntax ] [--nocheck] "
+            << " [ -h | --help ] [ -p | --trace_parsing ] [ -s | --trace_scanning ] [ -emit-mir ] [ -emit-ast ] [-nocheck] [-o <output-file>]"
             << "<input-file>"
             << std::endl;
 }
@@ -19,9 +19,10 @@ int main(int argc, char *argv[])
     ErrorReporter reporter(std::cerr);
     SyntaxTreeChecker checker(reporter);
 
-    bool print = false;
+    bool print_ast = false;
     bool print_IR = false;
     bool check = true;
+    std::string out_file = "a.ll";
     std::string filename = "test.sysy";
     for (int i = 1; i < argc; ++i) {
         if (argv[i] == std::string("-h") || argv[i] == std::string("--help")) {
@@ -34,17 +35,19 @@ int main(int argc, char *argv[])
             driver.trace_scanning = true;
         else if (argv[i] == std::string("-emit-mir"))
             print_IR = true;
-        else if (argv[i] == std::string("-e") || argv[i] == std::string("--emit_syntax"))
-            print = true;
-        else if (argv[i] == std::string("--nocheck"))
+        else if (argv[i] == std::string("-emit-ast"))
+            print_ast = true;
+        else if (argv[i] == std::string("-nocheck"))
             check = false;
+        else if (argv[i] == std::string("-o"))
+            out_file = argv[++i];
         else{
             filename = argv[i];
         }
     }
 
     auto root = driver.parse(filename);
-    if (print)
+    if (print_ast)
         root->accept(printer);
     if(check){
         root->accept(checker);
@@ -56,6 +59,13 @@ int main(int argc, char *argv[])
     }
     if (print_IR) {
         root->accept(builder);
+        auto m = builder.getModule();
+        m->set_print_name();
+        auto IR = m->print();
+        std::ofstream output_stream;
+        output_stream.open(out_file, std::ios::out);
+        output_stream << IR;
+        output_stream.close();
     }
     return 0;
 }
