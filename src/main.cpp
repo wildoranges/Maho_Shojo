15f5c3085378874a62ffd7c4b95444ed71cd4a99
ZQHuang <1170744043@qq.com>
2021-07-26 16:15:37 +0800

    fix bugs about ActiveVar

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "DominateTree.h"
 #include "mem2reg.h"
 #include "LIR.h"
+#include "ActiveVar.h"
 
 void print_help(const std::string& exe_name) {
   std::cout << "Usage: " << exe_name
@@ -81,6 +82,7 @@ int main(int argc, char *argv[])
         std::cout << "Mem2Reg\n";
 #endif
         passmgr.addPass<LIR>();
+        passmgr.addPass<ActiveVar>();
         m->set_print_name();
         passmgr.execute();
 #ifdef DEBUG
