0e4794078e84348ac803ac804574167fad967f3b
ZQHuang <1170744043@qq.com>
2021-07-29 14:32:00 +0800

    add flag to control optimization

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 
 void print_help(const std::string& exe_name) {
   std::cout << "Usage: " << exe_name
-            << " [ -h | --help ] [ -p | --trace_parsing ] [ -s | --trace_scanning ] [ -emit-mir ] [ -emit-ast ] [-nocheck] [-o <output-file>] "
+            << " [ -h | --help ] [ -p | --trace_parsing ] [ -s | --trace_scanning ] [ -emit-mir ] [ -emit-ast ] [-nocheck] [-o <output-file>] [ -O0 ]"
             << "<input-file>"
             << std::endl;
 }
@@ -33,6 +33,7 @@ int main(int argc, char *argv[])
     bool print_ast = false;
     bool print_IR = false;
     bool check = true;
+    bool no_optimize = false;
     std::string out_file = "a.ll";
     std::string filename = "test.sy";
     for (int i = 1; i < argc; ++i) {
@@ -52,7 +53,10 @@ int main(int argc, char *argv[])
             check = false;
         else if (argv[i] == std::string("-o"))
             out_file = argv[++i];
-        else{
+        else if (argv[i] == std::string("-O0")) {
+            no_optimize = true;
+        }
+        else {
             filename = argv[i];
         }
     }
@@ -71,36 +75,59 @@ int main(int argc, char *argv[])
     if (print_IR) {
         root->accept(builder);
         auto m = builder.getModule();
-#ifdef DEBUG
-        std::cout << "module\n";
-#endif
-        PassMgr passmgr(m.get());
-#ifdef DEBUG
-        std::cout << "passmgr\n";
-#endif
-        passmgr.addPass<DominateTree>();
-        passmgr.addPass<RDominateTree>();
-#ifdef DEBUG
-        std::cout << "DomTree\n";
-#endif
-        passmgr.addPass<Mem2Reg>();
-#ifdef DEBUG
-        std::cout << "Mem2Reg\n";
-#endif
-        //passmgr.addPass<LIR>();
-        passmgr.addPass<ActiveVar>();
-        passmgr.addPass<ConstPropagation>();
-        passmgr.addPass<DeadCodeElimination>();
-        passmgr.addPass<CFGSimplifier>();
-        passmgr.addPass<CFG_analyse>();
-        /****passmgr.addPass<CFG_analyse>();****
-         ***this is executed in LoopInvariant***/
-        passmgr.addPass<LoopInvariant>();
+        if (no_optimize == false) {
+            PassMgr passmgr(m.get());
 
-        passmgr.addPass<LIR>();
+            passmgr.addPass<CFGSimplifier>();
 
-        m->set_print_name();
-        passmgr.execute();
+            passmgr.addPass<DeadCodeElimination>();
+
+            passmgr.addPass<DeadCodeElimination>();
+            passmgr.addPass<CFGSimplifier>();
+
+            passmgr.addPass<DominateTree>();
+            passmgr.addPass<RDominateTree>();
+            passmgr.addPass<Mem2Reg>();
+            passmgr.addPass<DeadCodeElimination>();
+
+            passmgr.addPass<ConstPropagation>();
+            passmgr.addPass<DeadCodeElimination>();
+            passmgr.addPass<CFGSimplifier>();
+
+            passmgr.addPass<DeadCodeElimination>();
+            passmgr.addPass<CFGSimplifier>();
+
+            passmgr.addPass<CFGSimplifier>();
+            passmgr.addPass<ConstPropagation>();
+            passmgr.addPass<DeadCodeElimination>();
+            passmgr.addPass<CFGSimplifier>();
+
+            passmgr.addPass<DeadCodeElimination>();
+            passmgr.addPass<CFGSimplifier>();
+
+            passmgr.addPass<LoopInvariant>();
+            passmgr.addPass<CFGSimplifier>();
+
+            passmgr.addPass<DeadCodeElimination>();
+            passmgr.addPass<CFGSimplifier>();
+
+            passmgr.addPass<DeadCodeElimination>();
+            passmgr.addPass<CFGSimplifier>();
+
+            passmgr.addPass<DeadCodeElimination>();
+
+            //passmgr.addPass<LIR>();
+            passmgr.addPass<DeadCodeElimination>();
+            passmgr.addPass<DeadCodeElimination>();
+
+            passmgr.addPass<ActiveVar>();
+            //passmgr.addPass<CFG_analyse>();
+            /****passmgr.addPass<CFG_analyse>();****
+             ***this is executed in LoopInvariant***/
+            m->set_print_name();
+            passmgr.execute();
+        }
+
 #ifdef DEBUG
         std::cout << "exec\n";
 #endif
