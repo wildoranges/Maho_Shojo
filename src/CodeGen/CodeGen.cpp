56ab6ce95c738fad4955b5e1a64d765b9a542342
wildoranges <1294214414@qq.com>
2021-07-30 19:49:51 +0800

    fix bugs

--- src/CodeGen/CodeGen.cpp
+++ src/CodeGen/CodeGen.cpp
@@ -492,6 +492,10 @@ namespace CodeGen{
 
     std::string bb_gen(BasicBlock* bb){
         std::string code;
+        code += bb_label[bb].get_code()+":"+IR2asm::endl;
+        for(auto inst : bb->get_instructions()){
+            code += instr_gen(inst);
+        }
         //TODO: instruction gen
         return code;
     }
