6fa19b8b94086c061b988a7f5e511f0cfb90abd3
ZQHuang <1170744043@qq.com>
2021-08-04 20:23:21 +0800

    fix bugs about ActiveVar

--- src/Optimize/ActiveVar.cpp
+++ src/Optimize/ActiveVar.cpp
@@ -73,6 +73,18 @@ void ActiveVar::get_live_in_live_out() {
             std::set<Value *> tmp_live_out = {};
             for (auto succBB : bb->get_succ_basic_blocks()) {
                 auto succ_tmp_live_in = live_in[succBB];
+                std::set<Value *> active_phi_val = {};
+                for (auto instr : succBB->get_instructions()) {
+                    if (instr->is_phi()) {
+                        for (int i = 1; i < instr->get_num_operand(); i+=2) {
+                            if (instr->get_operand(i) == bb) {
+                                if (active_phi_val.find(instr->get_operand(i - 1)) == active_phi_val.end()) {
+                                    active_phi_val.insert(instr->get_operand(i - 1));
+                                }
+                            }
+                        }
+                    }
+                }
                 for (auto instr : succBB->get_instructions()) {
                     if (instr->is_phi()) {
                         for (int i = 1; i < instr->get_num_operand(); i+=2) {
@@ -84,6 +96,7 @@ void ActiveVar::get_live_in_live_out() {
                         }
                     }
                 }
+                std::set_union(succ_tmp_live_in.begin(), succ_tmp_live_in.end(), active_phi_val.begin(), active_phi_val.end(), std::inserter(succ_tmp_live_in, succ_tmp_live_in.begin()));
                 std::set_union(tmp_live_out.begin(), tmp_live_out.end(), succ_tmp_live_in.begin(), succ_tmp_live_in.end(), std::inserter(tmp_live_out, tmp_live_out.begin()));
             }
             // 迭代后的in和out必不可能小于迭代前的in和out(归纳法可证)
