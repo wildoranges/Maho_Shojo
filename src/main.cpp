ebbaeacfe18d02ac47a302fd48ce2ae80ee8ccc6
wildoranges <zkd18cjb@mail.ustc.edu.cn>
2021-08-06 14:12:18 +0800

    fix ava expr

--- src/main.cpp
+++ src/main.cpp
@@ -139,6 +139,7 @@ int main(int argc, char *argv[])
             passmgr.addPass<DeadCodeElimination>();
             passmgr.addPass<LIR>();
             passmgr.addPass<DeadCodeElimination>();
+            passmgr.addPass<AvailableExpr>();
             passmgr.addPass<ActiveVar>();
             passmgr.addPass<CFG_analyse>();
 
