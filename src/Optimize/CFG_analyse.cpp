0357f68b79f6c6d957610c50d3f3dbcbc4c7ca08
tinysnail <abyss@mail.ustc.edu.cn>
2021-08-12 21:21:16 +0800

    tarjan nmsl

--- src/Optimize/CFG_analyse.cpp
+++ src/Optimize/CFG_analyse.cpp
@@ -46,10 +46,10 @@ std::vector<BasicBlock*>* BBs;
 std::stack<std::vector<BasicBlock*>*> loop_stack;
 
 void CFG_analyse::loop_find(Function *func){
-    color.clear();
     DFN = 0;
     BB_DFN.clear();
     BB_LOW.clear();
+    color.clear();
     tarjan_DFS(func->get_entry_block());
     while(!loop_stack.empty()){
         auto loop = loop_stack.top();
@@ -61,12 +61,12 @@ void CFG_analyse::loop_find(Function *func){
             BB->loop_depth_add();
             bb_loop[BB] = loop;
         }
-        color[find_loop_entry(loop)] = 1;
+        color[find_loop_entry(loop)] = 3;
         DFN = 0;
         BB_DFN.clear();
         BB_LOW.clear();
         for (auto succ : find_loop_entry(loop)->get_succ_basic_blocks()){
-            if (bb_loop[succ] == loop && succ != find_loop_entry(loop)){
+            if (bb_loop[succ] == loop && color[succ]!=3){
                 tarjan_DFS(succ);
             }
         }
@@ -79,13 +79,16 @@ void CFG_analyse::tarjan_DFS(BasicBlock *BB){
     BB_DFN[BB] = DFN;
     BB_LOW[BB] = DFN;
     BB_Stack.push(BB);
-    color[BB] = 0;
+    color[BB] = 1;
     for (auto succ : BB->get_succ_basic_blocks()){
-        if (color[succ] != 1){
+        if (color[succ] != 3){
             if (BB_DFN[succ] == 0){
                 tarjan_DFS(succ);
+                if (BB_LOW[succ] < BB_LOW[BB]){
+                    BB_LOW[BB] = BB_LOW[succ];
+                }
             }
-            if (BB_LOW[succ] < BB_LOW[BB] && color[succ]<1){
+            else if (BB_LOW[succ] < BB_LOW[BB] && color[succ] == 1){
                 BB_LOW[BB] = BB_LOW[succ];
             }
         }
@@ -95,16 +98,13 @@ void CFG_analyse::tarjan_DFS(BasicBlock *BB){
         auto bb_instack = BB_Stack.top();
         while (bb_instack != BB){
             BB_Stack.pop();
-            BBs->push_back(bb_instack);
             color[bb_instack] = 2;
-            if (BB_Stack.empty()){
-                break;
-            }
+            BBs->push_back(bb_instack);
             bb_instack = BB_Stack.top();
         }
         BB_Stack.pop();
-        BBs->push_back(bb_instack);
         color[bb_instack] = 2;
+        BBs->push_back(bb_instack);
         if (BBs->size()>1){
             loops.push_back(BBs);
             loop_stack.push(BBs);
