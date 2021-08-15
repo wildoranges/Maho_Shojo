3ce2d833a1025025a05b1c17e228220618b1ffef
ZQHuang <1170744043@qq.com>
2021-08-15 20:11:07 +0800

    fix bugs

--- src/Optimize/CFGSimplifier.cpp
+++ src/Optimize/CFGSimplifier.cpp
@@ -100,6 +100,13 @@ bool CFGSimplifier::delete_unreachable_bb() {
                 if (instr->is_phi()) {
                     for (int i = 1; i < instr->get_num_operand(); i+=2) {
                         if (instr->get_operand(i) == unreachable_bb) {
+                            for (int j = i + 1; j < instr->get_num_operand(); j++) {
+                                for (auto &use : instr->get_operand(j)->get_use_list()) {
+                                    if (use.val_ == instr && use.arg_no_ == j) {
+                                        use.arg_no_ -= 2;
+                                    }
+                                }
+                            }
                             instr->remove_operands(i - 1, i);
                         }
                     }
@@ -232,6 +239,13 @@ void CFGSimplifier::combine_bb(BasicBlock *bb, BasicBlock *succ_bb) {
                         instr->replace_all_use_with(instr->get_operand(0));
                         wait_delete_instr.push_back(instr);
                     } else {
+                        for (int j = i + 1; j < instr->get_num_operand(); j++) {
+                            for (auto &use : instr->get_operand(j)->get_use_list()) {
+                                if (use.val_ == instr && use.arg_no_ == j) {
+                                    use.arg_no_ -= 2;
+                                }
+                            }
+                        }
                         instr->remove_operands(i - 1, i);
                         i -= 2;
                         if (instr->get_num_operand() == 2  && succ_bb->get_pre_basic_blocks().size() == 0) {
